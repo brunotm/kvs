e6a6d43a9ecb8a9d151a6acad91867a754333058
Bruno Moura <brunotm@gmail.com>
2017-12-04 23:29:45 +0000

    Adjust testing
    Remove unnecessary tests
    Add batch tests

--- goleveldb/goleveldb_test.go
+++ goleveldb/goleveldb_test.go
@@ -5,7 +5,6 @@ import (
 	"testing"
 	"time"
 
-	"github.com/brunotm/kvs"
 	"github.com/matryer/is"
 )
 
@@ -69,14 +68,6 @@ func TestCRUD(t *testing.T) {
 	is.NoErr(err)
 	is.True(exists == false)
 
-	// Try set with empty key
-	err = store.Set(testValue, "")
-	is.True(err == kvs.ErrBadKey)
-
-	// Try get with empty key
-	_, err = store.Get("")
-	is.True(err == kvs.ErrBadKey)
-
 	is.NoErr(store.Remove())
 }
 
@@ -101,27 +92,6 @@ func TestCRUDTTL(t *testing.T) {
 
 	// Check if expired key exists
 	exists, err = store.Has(testKey)
-	is.NoErr(err)
-	is.True(exists == false)
-
-	// Set a key/value with ttl
-	err = store.SetWithTTL(testValue, time.Millisecond*2, testKey)
-	is.NoErr(err)
-
-	// Get key/value
-	value, err := store.Get(testKey)
-	is.NoErr(err)
-	is.True(string(value) == string(testValue))
-
-	// Sleep beyond ttl
-	time.Sleep(time.Millisecond * 3)
-
-	// Expire
-	err = store.expire()
-	is.NoErr(err)
-
-	// Check if key exists within leveldb
-	exists, err = store.db.Has([]byte(`testKey`), ropt)
 	is.NoErr(err)
 	is.True(exists == false)
 
@@ -162,6 +132,30 @@ func TestCRUDTree(t *testing.T) {
 	is.NoErr(err)
 	is.True(exists == false)
 
+	is.NoErr(store.Remove())
+}
+
+func TestBatch(t *testing.T) {
+	is := is.New(t)
+
+	// Create a new store
+	store, err := New(testPath)
+	is.NoErr(err)
+
+	batch := store.NewBatch()
+	for x := 0; x < 100; x++ {
+		k := testKey + strconv.Itoa(x)
+		batch.Set(testValue, k)
+	}
+	is.NoErr(batch.Write())
+
+	for x := 0; x < 100; x++ {
+		k := testKey + strconv.Itoa(x)
+		batch.Delete(k)
+	}
+	is.NoErr(batch.Write())
+
+	// Remove a closed store
 	is.NoErr(store.Remove())
 }
 
